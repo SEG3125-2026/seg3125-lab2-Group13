2ce566ae5029a32ee877e5e31aa7e6675c25da0b
Charlie2hang <145156395+Charlie2hang@users.noreply.github.com>
2026-01-26 22:43:49 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+"use strict";
 
+function $(id) {
+  return document.getElementById(id);
+}
+
+function formatPrice(value) {
+  const num = Number(value);
+  if (!Number.isFinite(num)) return "$0.00";
+  return new Intl.NumberFormat("en-CA", { style: "currency", currency: "CAD" }).format(num);
+}
